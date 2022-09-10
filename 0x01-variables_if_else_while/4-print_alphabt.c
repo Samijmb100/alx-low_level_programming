44a31ed366682c5212a1300922eef7cefd208504
Samuel <samueldavidm10@gmail.com>
2022-09-09 17:11:20 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Ommit some alphabeth
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != q || c != e)
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
