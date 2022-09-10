7dba4dd60cec4791c4506a659b2b69cf34fde1cd
Samuel <samueldavidm10@gmail.com>
2022-09-09 17:52:57 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio>
+
+/**
+ * main - print number
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
