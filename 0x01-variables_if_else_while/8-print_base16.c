b35600b07938f831ed8e4d9a5e1e11e857069ec9
Samuel <samueldavidm10@gmail.com>
2022-09-09 18:17:29 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - base 16 lowercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
