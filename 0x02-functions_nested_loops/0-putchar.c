1186b0962ef980c1ac212eccb0877a9f25e8dbfc
Samuel <samueldavidm10@gmail.com>
2022-09-13 11:31:04 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,8 +7,13 @@
  */
 int main(void)
 {
-	char c = _putchar;
-
-	_putchar("%d", c);
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
 	return (0);
 }
