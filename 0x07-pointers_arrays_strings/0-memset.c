0dcf2b7ae8849afbf9cf9078d2726185cd3ddc42
Samuel <samueldavidm10@gmail.com>
2022-09-26 11:30:05 -0700

    Manipulation illegally

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _memset - allocate constant byte
+ * memset - Main function
+ * @s: character
+ * @b: character
+ * @n: size of memory to be allocated
+ * Return: pointer char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char memset(char *s, char b, unsigned int n)
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+More pointers and more complex arrays 2D
