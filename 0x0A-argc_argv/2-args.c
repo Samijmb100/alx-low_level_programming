b9f0c4f563a917bb8211309e9e2dcc44922d94eb
Samuel <samueldavidm10@gmail.com>
2022-10-01 16:15:52 -0700

    th

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function
+ * @argc: param
+ * @argv: param
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
